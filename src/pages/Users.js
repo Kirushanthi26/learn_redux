c8a9094fa27acacda6ff3b27cb62b187cd601fe5
kirushanthi26 <kirushanthikiru26@gmail.com>
2023-05-16 14:34:21 +0530

    reducer to users page

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Users = () => {
+
+    const users = useSelector ((state) => {
+        return state.users
+    })
+
+    
+
   return (
     <div>
       <h1>Users</h1>
