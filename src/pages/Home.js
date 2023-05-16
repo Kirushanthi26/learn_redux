5a409b12fa673ecd0bad837d3f708504730ebd20
kirushanthi26 <kirushanthikiru26@gmail.com>
2023-05-16 14:07:35 +0530

    add form

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Home = () => {
-  return (
-    <div>
-      <h1>home</h1>
-    </div>
-  )
-}
+  const [user, setUser] = useState({
+    id: "",
+    name: "",
+    password: "",
+  });
 
-export default Home
+  const inputHandler = (e) => {
+    const { name, value } = e.target;
+    setUser((prevItems) => {
+      return {
+        ...prevItems,
+        [name]: value,
+      };
+    });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(user);
+
+    setUser({
+      id: "",
+      name: "",
+      password: "",
+    });
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <h1>Home</h1>
+      <h3>Add User Form</h3>
+      <input
+        name="id"
+        type="number"
+        placeholder="Enter ID"
+        onChange={inputHandler}
+        value={user.id}
+      />
+      <input
+        name="name"
+        type="text"
+        placeholder="Enter user name"
+        onChange={inputHandler}
+        value={user.name}
+      />
+      <input
+        name="password"
+        type="password"
+        placeholder="Enter password"
+        onChange={inputHandler}
+        value={user.password}
+      />
+      <button type="submit">add user</button>
+    </form>
+  );
+};
+
+export default Home;
